87a365b7c6d52abf5696750460f8d2626f495f45
Angelo Perera <1932043+aceslick911@users.noreply.github.com>
2025-03-03 11:17:08 +1100

    dev: layout cleanup

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -1,80 +1,26 @@
 import './global.scss';
 
 import React from 'react';
-import logoUrl from '../assets/logo.svg';
-import { Link } from '../components/Link.js';
 
-import Header from '#V2/Header';
+import Header from '#V2/Header/Header';
+import TestContents from './testTemplate/TestTemplate';
 import { styled } from 'styled-components';
-import Footer from '#V2/Footer';
+import Footer from '#V2/Footer/Footer';
 
-export default function LayoutDefault({ children }: { children: React.ReactNode }) {
+export default function LayoutDefault(props: { children: React.ReactNode }) {
+  const { children } = props;
   return (
-    <Root>
+    <>
       <Header />
-      <Sidebar>
-        <Logo />
-        <Link href="/">Welcome</Link>
-        <Link href="/todo">Todo</Link>
-        <Link href="/star-wars">Data Fetching</Link>
-      </Sidebar>
-      <Content>{children}</Content>
+      <PageContents>
+        <TestContents>{children}</TestContents>
+      </PageContents>
       <Footer />
-    </Root>
+    </>
   );
 }
 
-function Sidebar({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      id="sidebar"
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        lineHeight: '1.8em',
-        borderRight: '2px solid #eee',
-      }}
-    >
-      {children}
-    </div>
-  );
-}
-
-function Content({ children }: { children: React.ReactNode }) {
-  return (
-    <div id="page-container">
-      <div
-        id="page-content"
-        style={{
-          padding: 20,
-          paddingBottom: 50,
-          minHeight: '100vh',
-        }}
-      >
-        {children}
-      </div>
-    </div>
-  );
-}
-
-function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10,
-      }}
-    >
-      <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
-      </a>
-    </div>
-  );
-}
-
-const Root = styled.div`
+const PageContents = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-grow: 1;
   `;
